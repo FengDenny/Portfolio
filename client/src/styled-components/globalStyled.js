e626113b02fe1f236db1cbf09b91c97856f54750
FengDenny <fengdennyy@gmail.com>
2021-12-21 19:14:53 -0800

    created new styling

--- client/src/styled-components/globalStyled.js
+++ client/src/styled-components/globalStyled.js
@@ -1,4 +1,5 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+import { MediaQueries } from "./styled";
 
 export const GlobalStyles = createGlobalStyle`
 
@@ -7,30 +8,116 @@ export const GlobalStyles = createGlobalStyle`
     margin:0;
     padding:0;
 }
+a {
+    list-style-type: none ;
+    text-decoration: none ;
+  }
 
 html,body{
     background:var(--bg-color);
     font-family:var(--primary-font);
 }
-
+body{
+    overflow-x:hidden;
+}
 :root{
     --bg-color: #F6F9FC;
-    --primary-color: #333333
-    --secondary-color: #0B95AB;
+    --primary-color: #333333;
+    --primary-color-hover: #2E2E2E;
+    --secondary-color: #0B95AB;  
+    --white-color:#fff;
     --data-suvery-color: #7796FE;
     --primary-font: 'Nunito', sans-serif;
+    --border-radius: 10px;
+    --drop-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
+    --text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    --span-text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1);
+   
+  
 }
 `;
 
 export const theme = {
   fontWeight: "bold",
-  fontSizeSm: "20px",
+  fontWeightRegular: "200",
+  fontSizeXSM: "18px",
+  fontSizeSM: "20px",
   fontSizeMD: "30px",
+  fontSizeLG: "40px",
+  fontSizeHero: "100px",
   primaryColor: "var(--primary-color)",
   secondaryColor: "var(--secondary-color)",
   height: "100vh",
   justifyContent: "space-between",
+  width: "120px",
+  buttonHeight: "30px",
+  flexDirection: "column",
+  flexDirectionRow: "row",
 };
+
+// Type Effect
+
+const type = keyframes`
+0% {
+    width: 0;
+  }
+  99.9% {
+    border-right: .1em solid var(--primary-color);
+  }
+  100% {
+    border: none;
+  }
+  @-webkit-keyframes type {
+    0% {
+      width: 0;
+    }
+    99.9% {
+      border-right: .1em solid var(--primary-color);
+    }
+    100% {
+      border: none;
+    }
+  }
+`;
+const type2 = keyframes`
+0% {
+    width: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  99.9% {
+    border-right: .1em solid var(--primary-color);
+  }
+  100% {
+    opacity: 1;
+    border: none;
+  }
+  
+  @-webkit-keyframes type2 {
+    0% {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    99.9% {
+      border-right: .1em solid var(--primary-color);
+    }
+    100% {
+      opacity: 1;
+      border: none;
+    }
+}
+`;
+const type3 = keyframes`
+to   { visibility: visible; }
+  
+  @-webkit-keyframes type3 {
+    to   { visibility: visible; }
+  }
+`;
+
 // Containers
 export const Container = styled.div`
   display: grid;
@@ -47,13 +134,101 @@ export const Container = styled.div`
   padding: 0;
 `;
 
-// Headings/ Paragraphs
+// Headings/ Paragraphs/ Span
 export const H1 = styled.h1`
   font-weight: ${(props) => props.theme.fontWeight};
   color: ${(props) => props.theme.primaryColor};
 `;
+export const HeroH1 = styled(H1)`
+  font-size: ${(props) => props.theme.fontSizeHero};
+  text-shadow: var(--text-shadow);
+  // animation start
+  width: 100%;
+  -webkit-animation: ${type} 2s steps(40, end);
+  animation: ${type} 2s steps(40, end);
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+  border-right: 0.1em solid var(--primary-color);
+  white-space: nowrap;
+  overflow: hidden;
+`;
 
-export const Paragraphs = styled.p`
-color:${(props) => props.theme.primaryColor}
-font-weight:${(props) => props.theme.fontWeight}
+export const Paragraph = styled.p`
+  color: ${(props) => props.theme.primaryColor};
+  font-weight: ${(props) => props.theme.fontWeightRegular};
+  font-size: ${(props) => props.theme.fontSizeMD};
+  padding-top: 1rem;
+  margin-left: 0.5rem;
+  //animation start
+  width: 45rem;
+  opacity: 0;
+  -webkit-animation: ${type2} 2s steps(40, end);
+  animation: ${type2} 2s steps(40, end);
+  -webkit-animation-delay: 2s;
+  animation-delay: 2s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+  border-right: 0.1em solid var(--primary-color);
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+export const Span = styled.span`
+  color: ${(props) => props.theme.secondaryColor};
+  text-shadow: var(--span-text-shadow);
+  font-weight: ${(props) => props.theme.fontWeight};
+`;
+
+// Button
+
+export const ButtonPrimary = styled.button`
+  position: relative;
+  top: 0.2rem;
+  font-size: ${(props) => props.theme.fontSizeXSM};
+  background: ${(props) => props.theme.primaryColor};
+  color: var(--white-color);
+  border: 1px solid var(--primary-color);
+  border-radius: var(--border-radius);
+  box-shadow: var(--drop-shadow);
+
+  width: ${(props) => props.theme.width};
+  height: ${(props) => props.theme.buttonHeight};
+  cursor: pointer;
+  :hover {
+    background: ${(props) => props.theme.secondaryColor};
+    border: 1px solid var(--secondary-color);
+  }
+  ${MediaQueries("laptop")`
+  margin-top: 0.5rem;
+  left: 1rem;
+  position: relative;
+  `}
+`;
+
+// Flex
+export const DisplayFlex = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.theme.flexDirection};
+`;
+
+export const DisplayFlexRow = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.theme.flexDirectionRow};
+`;
+
+// Hero
+export const HeroLink = styled.a`
+  position: relative;
+  font-size: ${(props) => props.theme.fontSizeMD};
+  color: ${(props) => props.theme.primaryColor};
+  font-weight: ${(props) => props.theme.fontWeight};
+  padding: 1rem;
+  width: 70px;
+  top: 5px;
+  :hover {
+    color: var(--secondary-color);
+  }
+  //   animations
+  visibility: hidden;
+  animation: ${type3} 0s 4s forwards;
 `;
--- client/src/styled-components/styled.js
+++ client/src/styled-components/styled.js
@@ -1,6 +1,22 @@
 import styled from "styled-components";
 import Modal, { BaseModalBackground } from "styled-react-modal";
 
+// media queries
+
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const MediaQueries = (key) => {
+  return (style) => `@media (max-width: ${size[key]}){${style}}`;
+};
+
 // Modal
 export const FadingBackground = styled(BaseModalBackground)`
   opacity: ${(props) => props.opacity};
@@ -9,13 +25,25 @@ export const FadingBackground = styled(BaseModalBackground)`
 
 // Main
 export const Main = styled.main`
-  height: ${(props) => props.theme.height};
   grid-area: main;
   padding: 0.25rem;
+  margin: 10rem;
 `;
 
 // Navbar
+
+export const Nav = styled.nav`
+  position: relative;
+  right: 20rem;
+  ${MediaQueries("laptop")`
+  right:0px;
+  display: flex;
+flex-direction: column;
+`}
+`;
+
 export const NavbarContainer = styled.nav`
+  position: relative;
   grid-area: nav;
   display: flex;
   flex-direction: row;
@@ -27,4 +55,81 @@ export const NavbarContainer = styled.nav`
 export const NavbarLogo = styled.img`
   width: auto;
   height: 30px;
+`;
+export const NavLink = styled.a`
+  position: relative;
+  font-size: ${(props) => props.theme.fontSizeXSM};
+  color: ${(props) => props.theme.primaryColor};
+  font-weight: ${(props) => props.theme.fontWeight};
+  padding: 1rem;
+  top: 5px;
+  :hover {
+    text-decoration: underline;
+    color: var(--secondary-color);
+  }
+  :active {
+    text-decoration: underline;
+  }
+`;
+
+export const StyledBurger = styled.button`
+  position: absolute;
+  top: 2%;
+  right: 2rem;
+  display: none;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+  &:focus {
+    outline: none;
+  }
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+    :first-child {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+      transition: "all 1s";
+      background: var(--primary-color);
+    }
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? "0" : "1")};
+      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
+      transition: "all 1s";
+      background: var(--primary-color);
+    }
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+      transition: "all 1s";
+      background: var(--primary-color);
+    }
+  }
+  ${MediaQueries("laptop")`
+  display: flex;
+  flex-direction: column;
+`}
+`;
+
+export const StyledMenu = styled.nav`
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.3s ease-in-out;
+
+  ${MediaQueries("laptop")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 20vh;
+  padding: 2rem;
+  margin-top:2rem;
+margin-right:2rem;
+  width:30%;
+`}
 `;
