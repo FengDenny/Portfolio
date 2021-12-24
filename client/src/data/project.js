56d9684aa4647d7c21132634306e0adad389c02a
FengDenny <fengdennyy@gmail.com>
2021-12-23 20:08:48 -0800

    project data

---
+++ client/src/data/project.js
@@ -0,0 +1,46 @@
+export const project = [
+  {
+    id: 1,
+    title: "Knewlys",
+    description:
+      "A wedding gallery app I have created to host my sisters wedding pictures. I designed using Figma tool, and MERN stack approach for development.",
+    demo: "https://knewlys-v1.herokuapp.com/",
+    github: "https://github.com/FengDenny/Knewlys_Deployment",
+    figma:
+      "https://www.figma.com/file/lMI1qgJyHuSdC3x3FTSY5h/Knewlys?node-id=148%3A57",
+    image: "knewlys.png",
+  },
+  {
+    id: 2,
+    title: "DataSurvey.",
+    description:
+      "This is Human Computer Interaction individual assignment that was created using User Centered Design approach. For designing, I used Figma. As my core framework for this project, I used React and Redux. I finished this off  off using Styled-Components to make it look clean and minimalist.",
+    demo: "https://fengdenny.github.io/projectform",
+    github: "https://github.com/FengDenny/projectform",
+    figma:
+      "https://www.figma.com/file/kKWKEQOtgsGNulv2u1ntwC/DataSurvey?node-id=0%3A1",
+    image: "dataSurvey.png",
+  },
+  {
+    id: 3,
+    title: "GatorDash",
+    description:
+      "A restaurant ordering service created with a team of 6 in Software Engineering class.  Creating our three use cases, and personas to design and develop our mockups. ",
+    demo: require("./ProductScreenshots.pdf"),
+    github: "https://github.com/FengDenny/Food-delivery-React-NodeJS-App",
+    figma:
+      "https://www.figma.com/file/zcSnuxaRagaZrSVq7tQlia/CSC648-Milestone2-Mockup-Rework?node-id=0%3A1",
+    image: "gatordash.png",
+  },
+  {
+    id: 4,
+    title: "Participaid",
+    description:
+      "This project was created with a team of 3 using User Centered Design approach. The team used Figma as our design tool. As our  core framework,  React and Redux was chosen to create the UX. To create the UI, React-Bootstrap was used.  ",
+    demo: "https://fengdenny.github.io/csc642participaid/",
+    github: "https://github.com/FengDenny/csc642participaid",
+    figma:
+      "https://www.figma.com/file/5orBOkFkjzB2Qp0wqSFJZk/Team1?node-id=17%3A4726",
+    image: "participaid.png",
+  },
+];
