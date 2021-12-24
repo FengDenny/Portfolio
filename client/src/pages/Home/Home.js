d4e5a523d03df6f321da2df14587d92cdca66019
FengDenny <fengdennyy@gmail.com>
2021-12-23 20:10:11 -0800

    added recent project section

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import RecentProjects from "./RecentProjects";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <RecentProjects />
+    </>
   );
 }
