32c26e26e741a528055976fb817fd619d0321031
FengDenny <fengdennyy@gmail.com>
2021-12-23 20:09:59 -0800

    added bottom position

--- client/src/pages/Home/Hero.js
+++ client/src/pages/Home/Hero.js
@@ -8,6 +8,7 @@ import {
   Paragraph,
   Span,
   HeroLink,
+  PositionBottomRelative,
 } from "../../styled-components/globalStyled";
 import { ThemeProvider } from "styled-components";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
@@ -16,21 +17,23 @@ export default function Hero() {
   return (
     <ThemeProvider theme={theme}>
       <Main>
-        <DisplayFlex>
-          <HeroH1>Hi! I'm Denny Feng.</HeroH1>
-          <Paragraph>
-            A Software Engineer utilizing <Span>React</Span> to build{" "}
-            <Span>UX</Span>/<Span>UI</Span>.
-          </Paragraph>
-          <DisplayFlexRow>
-            <HeroLink href='https://github.com/FengDenny'>
-              <FaGithub />
-            </HeroLink>
-            <HeroLink href='https://www.linkedin.com/in/denny-feng/'>
-              <FaLinkedin />
-            </HeroLink>
-          </DisplayFlexRow>
-        </DisplayFlex>
+        <PositionBottomRelative>
+          <DisplayFlex>
+            <HeroH1>Hi! I'm Denny Feng.</HeroH1>
+            <Paragraph>
+              A Software Engineer utilizing <Span>React</Span> to build{" "}
+              <Span>UX</Span>/<Span>UI</Span>.
+            </Paragraph>
+            <DisplayFlexRow>
+              <HeroLink href='https://github.com/FengDenny'>
+                <FaGithub />
+              </HeroLink>
+              <HeroLink href='https://www.linkedin.com/in/denny-feng/'>
+                <FaLinkedin />
+              </HeroLink>
+            </DisplayFlexRow>
+          </DisplayFlex>
+        </PositionBottomRelative>
       </Main>
     </ThemeProvider>
   );
