6b3cb635cf94d4153f08f9637e7547bd3cc15c36
FengDenny <fengdennyy@gmail.com>
2021-12-24 10:12:27 -0800

    resume added

--- client/src/utility/Navbar/Burger/BurgerMenu.js
+++ client/src/utility/Navbar/Burger/BurgerMenu.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { ButtonPrimary } from "../../../styled-components/globalStyled";
 import { NavLink, StyledMenu, Nav } from "../../../styled-components/styled";
-
+import ResumePDF from "../../../data/Denny Resume.pdf";
 export default function BurgerMenu({ open }) {
   return (
     <StyledMenu open={open}>
       <Nav>
         <NavLink href='#'>About</NavLink>
         <NavLink href='#'>Contact</NavLink>
-        <ButtonPrimary>Resume</ButtonPrimary>
+        <ButtonPrimary
+          onClick={() =>
+            window.open(ResumePDF, "_blank", "noopener", "noreferrer")
+          }
+        >
+          Resume
+        </ButtonPrimary>
       </Nav>
     </StyledMenu>
   );
