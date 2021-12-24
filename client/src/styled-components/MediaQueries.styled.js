697e6328b8fc644c40123735c473707ac725895f
FengDenny <fengdennyy@gmail.com>
2021-12-23 20:11:18 -0800

    Media Queries

---
+++ client/src/styled-components/MediaQueries.styled.js
@@ -0,0 +1,15 @@
+// media queries
+
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const MediaQueries = (key) => {
+  return (style) => `@media (max-width: ${size[key]}){${style}}`;
+};
