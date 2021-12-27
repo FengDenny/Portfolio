2eba8449d2b490b4db5a278de9a66264b2ade5f4
FengDenny <fengdennyy@gmail.com>
2021-12-26 16:06:11 -0800

    refactored to use framer-motion

--- client/src/utility/Navbar/Burger/NavBurger.js
+++ client/src/utility/Navbar/Burger/NavBurger.js
@@ -3,7 +3,15 @@ import { StyledBurger } from "../../../styled-components/styled";
 
 export default function NavBurger({ open, setOpen }) {
   return (
-    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+    <StyledBurger
+      open={open}
+      onClick={() => setOpen((open) => !open)}
+      animate={{
+        scale: open ? 0.8 : 1,
+        opacity: open ? 0.5 : 1,
+      }}
+      transition={{ type: "spring", bounce: 0, duration: 0.4 }}
+    >
       <div />
       <div />
       <div />
