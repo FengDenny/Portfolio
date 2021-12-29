c384ccbba39eb2f596a383d53ed73414ba57a196
FengDenny <fengdennyy@gmail.com>
2021-12-29 00:42:03 -0800

    mobile testing margins/grid

--- client/src/styled-components/globalStyled.js
+++ client/src/styled-components/globalStyled.js
@@ -101,7 +101,7 @@ export const Container = styled.div`
  `}
   ${MediaQueries("mobileL")`
   max-width:1400px;
-  grid-template-rows: 0.2fr 0.25fr 1fr auto;
+  grid-template-rows: 0.22fr 0.25fr 1fr auto;
  `}
 `;
 
