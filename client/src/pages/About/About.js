e77ae433427010a3d27dc688b4c33e6aa1dc476a
FengDenny <fengdennyy@gmail.com>
2021-12-28 20:05:49 -0800

    Create About.js

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { theme } from "../../styled-components/globalStyled";
+import { FooterContainer, NavLink, Main } from "../../styled-components/styled";
+import { ThemeProvider } from "styled-components";
+export default function About() {
+  return (
+    <Main>
+      <section className='about-section'>
+        <h2>About</h2>
+      </section>
+    </Main>
+  );
+}
