ba9732d2011f12a3fff90fb44928ec34a58cbc8b
FengDenny <fengdennyy@gmail.com>
2021-12-30 18:49:04 -0800

    Technical  section data

---
+++ client/src/data/technical.js
@@ -0,0 +1,51 @@
+export const technical = [
+  {
+    front: [
+      {
+        id: 1,
+        title: "React.js",
+        image: "react.png",
+      },
+      {
+        id: 2,
+        title: "Styled-Components",
+        image: "styled-components.png",
+      },
+
+      {
+        id: 3,
+        title: "Node.js",
+        image: "node.png",
+      },
+    ],
+  },
+  {
+    description: [
+      {
+        description_one: "Use latest libraries",
+        description_two: "Use numerous of technologies",
+        description_three: "Developing websites from the ground up ",
+      },
+    ],
+  },
+  {
+    back: [
+      {
+        id: 1,
+        title: "MySQL",
+        image: "mysql.png",
+      },
+
+      {
+        id: 2,
+        title: "Express.js",
+        image: "express.png",
+      },
+      {
+        id: 3,
+        title: "MongoDB",
+        image: "mongodb.png",
+      },
+    ],
+  },
+];
