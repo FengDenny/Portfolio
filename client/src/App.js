c2ba98a801dbdd538ac6600e09a4629c96b57c0a
FengDenny <fengdennyy@gmail.com>
2022-01-04 19:42:00 -0800

    removed about path

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,11 @@ import { GlobalStyles, Container } from "./styled-components/globalStyled";
 import { ModalProvider } from "styled-react-modal";
 import { FadingBackground } from "./styled-components/styled";
 import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
 
 function App() {
-  let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-  ]);
+  let element = useRoutes([{ path: "/", element: <Home /> }]);
 
   return (
     <ModalProvider backgroundComponent={FadingBackground}>
