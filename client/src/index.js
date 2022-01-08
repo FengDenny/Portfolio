e0c01096b2bb4d02c1f142dd315d520b91aedfc6
FengDenny <fengdennyy@gmail.com>
2022-01-08 13:49:11 -0800

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,42 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+// redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./redux/reducer/rootReducer";
+import thunk from "redux-thunk";
 
+// redux-persist
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { PersistGate } from "redux-persist/integration/react";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = composeWithDevTools(applyMiddleware(thunk))(createStore)(
+  persistedReducer
+);
+
+const persistor = persistStore(store);
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router basename={process.env.PUBLIC_URL}>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
