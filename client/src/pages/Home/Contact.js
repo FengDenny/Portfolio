5f494a1b8205e0c2d17c213c3e3fbea0c83f4baf
FengDenny <fengdennyy@gmail.com>
2022-01-10 00:03:29 -0800

    form validations

--- client/src/pages/Home/Contact.js
+++ client/src/pages/Home/Contact.js
@@ -3,11 +3,6 @@ import { ContactSection } from "../../styled-components/styled";
 import { HRLine, H2, theme } from "../../styled-components/globalStyled";
 import { ThemeProvider } from "styled-components";
 import ContactLayout from "../../components/Contact/ContactLayout";
-import {
-  cardVariants,
-  rotateVariants,
-} from "../../components/Variants/Variants";
-import { motion } from "framer-motion";
 import { contact } from "../../data/contact";
 import { env } from "../../data/api";
 import {
@@ -15,29 +10,58 @@ import {
   setUserLastName,
   setUserEmail,
   setUserDescription,
+  setUserSubmit,
 } from "../../redux/actions/formAction";
 import emailjs from "@emailjs/browser";
 import { useDispatch } from "react-redux";
+import {
+  emailValidation,
+  nameValidation,
+  textAreaValidation,
+} from "../../components/FormValidation/validation";
 export default function Contact() {
   const [first, setFirst] = useState("");
   const [last, setLast] = useState("");
   const [email, setEmail] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
-  const form = useRef();
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState();
+  const [descriptionError, setDescriptionError] = useState();
+  const formRef = useRef();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    first &&
+      nameValidation(first, "First name", "First Name", setFirstNameError);
+    last && nameValidation(last, "Last name", "Last Name", setLastNameError);
+    email && emailValidation(email, "Email address", setEmailError);
+    projectDescription &&
+      textAreaValidation(
+        projectDescription,
+        "Project description",
+        setDescriptionError
+      );
+  }, [first, last, email, projectDescription]);
 
   const handleSend = (e) => {
     e.preventDefault();
-    emailjs.sendForm(
-      env.service_id,
-      env.template_id,
-      form.current,
-      env.user_id
-    );
-    dispatch(setUserFirstName(first));
-    dispatch(setUserLastName(last));
-    dispatch(setUserEmail(email));
-    dispatch(setUserDescription(projectDescription));
+    emailjs
+      .sendForm(env.service_id, env.template_id, formRef.current, env.user_id)
+      .then(() => {
+        dispatch(setUserFirstName(first));
+        dispatch(setUserLastName(last));
+        dispatch(setUserEmail(email));
+        dispatch(setUserDescription(projectDescription));
+        dispatch(setUserSubmit(true));
+        setFirst(" ");
+        setLast(" ");
+        setEmail(" ");
+        setProjectDescription(" ");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <ThemeProvider theme={theme}>
@@ -55,7 +79,11 @@ export default function Contact() {
             setLast={setLast}
             setEmail={setEmail}
             setProjectDescription={setProjectDescription}
-            form={form}
+            formRef={formRef}
+            firstNameError={firstNameError}
+            lastNameError={lastNameError}
+            emailError={emailError}
+            descriptionError={descriptionError}
           />
         ))}
       </ContactSection>
