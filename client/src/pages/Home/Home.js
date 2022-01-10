c2f72e08fe1fe70fbf08c98a97ebb3f7399414b0
FengDenny <fengdennyy@gmail.com>
2022-01-10 00:03:11 -0800

    progression icon

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,10 +4,13 @@ import RecentProjects from "./RecentProjects";
 import Technical from "./Technical";
 import About from "./About";
 import Contact from "./Contact";
+import ProgressIcon from "../../components/ProgressIcon/ProgressIcon";
+import ScrollToTop from "../../components/TopScroll/TopScroll";
 
 export default function Home() {
   return (
     <>
+      <ProgressIcon />
       <Hero />
       <About />
       <RecentProjects />
